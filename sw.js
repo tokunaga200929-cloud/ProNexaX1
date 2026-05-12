1737b7051c5d57d5d0f786a86f60b0b195253b4b
tokunaga200929-cloud <tokunaga200929@icloud.com>
2026-05-13 07:29:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -39,7 +39,8 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       return cached || fetch(event.request).then((response) => {
-        if (response && response.status === 200 && response.type === 'basic') {
+        if (response && response.status === 200 && response.type === 'basic'
+            && event.request.method === 'GET') {
           const responseClone = response.clone();
           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
         }
