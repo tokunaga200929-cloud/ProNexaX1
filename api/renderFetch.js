77e3aad8dc94ad3dbeed829a7f028ff3ed79f2b0
tokunaga200929-cloud <tokunaga200929@icloud.com>
2026-05-13 22:07:45 +0900

    Remove userAgent from renderFetch.js

--- api/renderFetch.js
+++ api/renderFetch.js
@@ -51,9 +51,7 @@ async function fetchWithBrowserless(url) {
         waitUntil: 'networkidle2',
         timeout:   GOTO_TIMEOUT_MS,
       },
-      userAgent:
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) ' +
-        'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+      
     }),
     signal: AbortSignal.timeout(GOTO_TIMEOUT_MS + 5000),
   });
